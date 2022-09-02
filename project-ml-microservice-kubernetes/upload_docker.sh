eb25957fd172dbdc4235165418bd94164270bb04
Ubuntu <ubuntu@ip-172-31-13-175.ec2.internal>
2022-09-02 20:26:18 +0000

    output files

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=omonoboigbator/firstone
 # Step 2:  
 # Authenticate & tag
+echo "$DP" | docker login -u "omonoboigbator" --password-stdin
+docker tag firstone $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
